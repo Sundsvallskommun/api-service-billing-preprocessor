110fa1395c09cadb0d3dac2d04973971a8357a3c
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2023-07-28 11:12:33 +0200

    UF-5566: Switched to Sonar

--- src/main/java/se/sundsvall/billingpreprocessor/api/validation/ValidRecipient.java
+++ src/main/java/se/sundsvall/billingpreprocessor/api/validation/ValidRecipient.java
@@ -1,16 +1,14 @@
 package se.sundsvall.billingpreprocessor.api.validation;
 
-import jakarta.validation.Constraint;
-import jakarta.validation.Payload;
-
-import se.sundsvall.billingpreprocessor.api.validation.impl.ValidRecipientConstraintValidator;
-import se.sundsvall.billingpreprocessor.api.validation.impl.ValidRecipientConstraintValidator;
-
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import se.sundsvall.billingpreprocessor.api.validation.impl.ValidRecipientConstraintValidator;
 
 @Documented
 @Target({ ElementType.FIELD, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE })
