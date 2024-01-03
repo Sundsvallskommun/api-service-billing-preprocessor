625d521a6a5292285911d81d249e378ad01de9dd
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2024-01-03 14:29:32 +0100

    UF-6619: Add CI

--- src/test/java/se/sundsvall/billingpreprocessor/api/model/BillingRecordTest.java
+++ src/test/java/se/sundsvall/billingpreprocessor/api/model/BillingRecordTest.java
@@ -7,7 +7,6 @@ import static com.google.code.beanmatchers.BeanMatchers.hasValidBeanToString;
 import static com.google.code.beanmatchers.BeanMatchers.hasValidGettersAndSetters;
 import static com.google.code.beanmatchers.BeanMatchers.registerValueGenerator;
 import static java.time.OffsetDateTime.now;
-import static org.apache.commons.lang3.RandomUtils.nextInt;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -15,6 +14,7 @@ import static se.sundsvall.billingpreprocessor.api.model.enums.Status.REJECTED;
 import static se.sundsvall.billingpreprocessor.api.model.enums.Type.INTERNAL;
 
 import java.time.OffsetDateTime;
+import java.util.Random;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -23,7 +23,7 @@ class BillingRecordTest {
 
 	@BeforeAll
 	static void setup() {
-		registerValueGenerator(() -> now().plusDays(nextInt()), OffsetDateTime.class);
+		registerValueGenerator(() -> now().plusDays(new Random().nextInt()), OffsetDateTime.class);
 	}
 
 	@Test
