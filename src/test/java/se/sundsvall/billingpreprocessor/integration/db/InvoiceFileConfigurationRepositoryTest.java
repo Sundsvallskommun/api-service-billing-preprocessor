9199f73fa52ccc07608baa1a700bf2363420f70c
Joakim Schürer <joakim.schurer@sundsvall.se>
2024-03-07 11:02:12 +0100

    UF-7232: Corrected active profile for unit tests

--- src/test/java/se/sundsvall/billingpreprocessor/integration/db/InvoiceFileConfigurationRepositoryTest.java
+++ src/test/java/se/sundsvall/billingpreprocessor/integration/db/InvoiceFileConfigurationRepositoryTest.java
@@ -1,5 +1,8 @@
 package se.sundsvall.billingpreprocessor.integration.db;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,13 +10,11 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
+
 import se.sundsvall.billingpreprocessor.integration.db.model.InvoiceFileConfigurationEntity;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 @SpringBootTest
-@ActiveProfiles("it")
+@ActiveProfiles("junit")
 @Transactional
 @Sql(scripts = {
 	"/db/scripts/truncate.sql",
