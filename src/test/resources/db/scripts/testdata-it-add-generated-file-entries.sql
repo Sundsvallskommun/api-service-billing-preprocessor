f32fbbeddc23f13506ea5deb1bb891901ccdfd57
Joakim Schürer <joakim.schurer@sundsvall.se>
2024-03-25 10:48:11 +0100

    UF-7597: Extended IT-tests with test for ftp transfer

---
+++ src/test/resources/db/scripts/testdata-it-add-generated-file-entries.sql
@@ -0,0 +1,6 @@
+-------------------------------------
+-- Invoice files
+-------------------------------------
+INSERT INTO invoice_file (created, name, status, `type`, content)
+VALUES ('2024-03-21 13:17:00.000', 'EXTERNAL_FILE_20240321.txt', 'GENERATED', 'EXTERNAL', 'External content'),
+       ('2024-03-22 13:37:00.000', 'INTERNAL_FILE_20240322.txt', 'GENERATED', 'INTERNAL', 'Internal content');
