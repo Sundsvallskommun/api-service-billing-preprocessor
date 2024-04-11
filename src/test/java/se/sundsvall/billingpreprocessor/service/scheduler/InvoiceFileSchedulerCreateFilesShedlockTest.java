ab2fe1b2ccf646ed100c0311f6159e440733b4bd
Michael Wallner <Michael.Wallner@Sundsvall.se>
2024-04-11 09:52:24 +0200

    UF-7537: Solved test issue

--- src/test/java/se/sundsvall/billingpreprocessor/service/scheduler/InvoiceFileSchedulerCreateFilesShedlockTest.java
+++ src/test/java/se/sundsvall/billingpreprocessor/service/scheduler/InvoiceFileSchedulerCreateFilesShedlockTest.java
@@ -1,21 +1,5 @@
 package se.sundsvall.billingpreprocessor.service.scheduler;
 
-import static java.time.Clock.systemUTC;
-import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.within;
-import static org.awaitility.Awaitility.await;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
-import java.util.Map;
-
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,8 +9,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
-
 import se.sundsvall.billingpreprocessor.service.InvoiceFileService;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
+
+import static java.time.Clock.systemUTC;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.within;
+import static org.awaitility.Awaitility.await;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 @SpringBootTest(properties = {
 	"scheduler.createfiles.cron=* * * * * *", // Setup to execute every second
@@ -45,7 +44,18 @@ class InvoiceFileSchedulerCreateFilesShedlockTest {
 		@Bean
 		@Primary
 		public InvoiceFileService createMock() {
-			return Mockito.mock(InvoiceFileService.class);
+
+			final var mockBean =  Mockito.mock(InvoiceFileService.class);
+
+			// Let mock hang
+			doAnswer( invocation -> {
+				mockCalledTime = LocalDateTime.now();
+				await().forever()
+					.until(() -> false);
+				return null;
+			}).when(mockBean).createFiles();
+
+			return mockBean;
 		}
 	}
 
@@ -55,19 +65,10 @@ class InvoiceFileSchedulerCreateFilesShedlockTest {
 	@Autowired
 	private NamedParameterJdbcTemplate jdbcTemplate;
 
-	private LocalDateTime mockCalledTime;
+	private static LocalDateTime mockCalledTime;
 
 	@Test
 	void verifyShedLockForCreateFiles() throws InterruptedException {
-
-		// Let mock hang
-		doAnswer( invocation -> {
-			mockCalledTime = LocalDateTime.now();
-			await().forever()
-				.until(() -> false);
-			return null;
-		}).when(invoiceFileServiceMock).createFiles();
-
 		// Make sure scheduling occurs multiple times
 		await().until(() -> mockCalledTime != null && LocalDateTime.now().isAfter(mockCalledTime.plusSeconds(2)));
 
